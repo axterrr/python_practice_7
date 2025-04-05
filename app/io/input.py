840bba1740b0da1a94fb690d500b1620ce458fff
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-04-05 20:02:37 +0300

    empty function for input and output

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def read_from_console():
+    pass
+
+
+def read_from_file():
+    pass
+
+
+def read_from_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def write_to_console():
+    pass
+
+
+def write_to_file():
+    pass
+
+
+def write_to_pandas():
+    pass
